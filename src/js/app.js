3c78827e10694acfc8171718faef652e02a1115c
yurayarosh <yura12323@gmail.com>
2019-09-03 10:28:10 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+// import '@babel/polyfill';
+import './lib/polyfill';
+import sayHello from './lib/sayHello';
+import setHTMLClassNames from './components/setHTMLClassNames';
+import setLazy from './components/setLazy';
+
+$(() => {
+  sayHello();
+  setHTMLClassNames();
+  setLazy();
+});
