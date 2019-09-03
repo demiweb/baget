ffb0ff637931a200f2ad43f47c0c246a2f718a56
yurayarosh <yura12323@gmail.com>
2019-09-03 17:41:14 +0300

    add services

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,22 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 // import '@babel/polyfill';
 import './lib/polyfill';
 import sayHello from './lib/sayHello';
 import setHTMLClassNames from './components/setHTMLClassNames';
 import setLazy from './components/setLazy';
+import setTextareaHeight from './components/setTextareaHeight';
+import toggleMenu from './components/toggleMenu';
 
-$(() => {
+document.addEventListener('DOMContentLoaded', () => {
   sayHello();
   setHTMLClassNames();
   setLazy();
+  setTextareaHeight();
+  toggleMenu();
 });
+
+// $(() => {
+//   sayHello();
+//   setHTMLClassNames();
+//   setLazy();
+// });
